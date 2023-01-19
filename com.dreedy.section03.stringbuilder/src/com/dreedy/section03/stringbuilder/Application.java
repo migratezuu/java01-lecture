345abf119c3db0f0babfc4709de5767556070b99
woos <tjoeun@DESKTOP-PFGGPM8>
2023-01-19 12:31:33 +0900

    2

--- com.dreedy.section03.stringbuilder/src/com/dreedy/section03/stringbuilder/Application.java
+++ com.dreedy.section03.stringbuilder/src/com/dreedy/section03/stringbuilder/Application.java
@@ -4,9 +4,9 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		Fruit fruit = new Fruit();
+			
+			
+			Fruit fruit = new Fruit();
 		
 		fruit.sayName(1);
 		
--- petbnb/src/com/greedy/petbnb/Application.java
+++ petbnb/src/com/greedy/petbnb/Application.java
@@ -1,5 +1,9 @@
 package com.greedy.petbnb;
 
 public class Application {
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
